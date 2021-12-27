8d3262fe4dc1bd127b6b3921768185b0290965f7
Kristina <ktuzova1901@gmail.com>
2021-12-27 22:10:04 +0200

    js

--- task-01.js
+++ task-01.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { good }
+  } = event.currentTarget;
+    const form = document.querySelector(".order-list");
+    const item = document.createElement("li");
+    form.after(item)
+    item.textContent = `${good.value}`
+  
+
+  console.log(`li: ${good.value}`);
+  
+}
