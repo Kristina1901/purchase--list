9569c37694ffba42c6d07004f6e28e4d1a28e2d5
Kristina <ktuzova1901@gmail.com>
2021-12-28 00:25:13 +0200

    js

--- task-01.js
+++ task-01.js
@@ -10,9 +10,22 @@ function handleSubmit(event) {
     const form = document.querySelector(".order-list");
     const item = document.createElement("li");
     form.after(item)
-    item.textContent = `${good.value}`
+  item.textContent = `${good.value}`
+  item.style.color = "blue"
+  item.style.backgroundColor ="yellow"
   
 
-  console.log(`li: ${good.value}`);
+  
+}
+form.addEventListener("reset", handleReset);
+
+function handleReset(event) {
+  event.preventDefault();
+  const {
+    elements: { good }
+  } = event.currentTarget;
+   const item = document.querySelector("li");
+    item.remove()
+
   
 }
